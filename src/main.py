b407672faadf6278e050321f42ae8b7b54a44f94
JacksonBennett05 <jcb263@gmail.com>
2025-10-28 12:48:55 -0400

    Add main entry point

--- src/main.py
+++ src/main.py
@@ -1,3 +1,19 @@
-from scraper.lego_scraper import get_lego_price
-from utils.compare import find_cheapest
-from utils.formatter import format_output
+from scraper.lego_scraper import brickset_login, get_lego_price
+import os
+
+def main():
+  API_KEY = os.getenv("BRICKSET_API_KEY")
+  USERNAME = os.getenv("BRICKSET_USER")
+  PASSWORD = os.getenv("BRICKSET_PASS")
+
+  user_hash = brickset_login(API_KEY, USERNAME, PASSWORD)
+  if not user_hash:
+    print("Login failed — check your .env credentials.")
+    return
+
+  set_number = input("Enter LEGO set number: ").strip()
+  lego_set = get_lego_price(set_number, API_KEY, user_hash)
+  print(lego_set)
+  
+if __name__ =="__main__":
+  main()
