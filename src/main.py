48e23b3ab03eda7a3737b436c7ca9b68672a4c58
JacksonBennett05 <jcb263@gmail.com>
2025-11-02 20:36:48 -0500

    Compare Skeleton

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from scraper.lego_scraper import brickset_login, get_lego_price
+from utils.compare import find_cheapest
 import os
 
 def main():
@@ -20,7 +21,7 @@ def main():
     is_correct = input("Is this the correct set (Y/N)? ").upper().strip()
     if(is_correct == "Y"):
       correct = True
-  
+  print("Testing", lego_set.get("Price"))
   
 if __name__ =="__main__":
   main()
--- src/utils/compare.py
+++ src/utils/compare.py
@@ -1,2 +1,2 @@
 def find_cheapest(results):
-  
+  print("hi")
